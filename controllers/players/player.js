f18d8c1c9efa85edb9eefd930b37a0ce50f29e90
aaroncf2008 <aaroncf2008@gmail.com>
2022-09-15 17:47:03 -0400

    e

--- controllers/players/player.js
+++ controllers/players/player.js
@@ -3,16 +3,15 @@ exports.getPlayers = async (req, res) => {
 
     if (params[0].length >= 3 && params[0].length <= 16) {
         const { search } = req.body
-        return res.status(200)
+        return res.status(200).render('players/player',{
+            search: params[0]
+        
+        })
     }
     else () => {
         return res.status(200).redirect(`/`)
     }
     
-    res.status(200).render('players/player',{
-        search: params[0]
-    
-    })
 }
 
 exports.postPlayers = async (req, res) => {
